a87504ee6fcaca5bbb2b6dcb32c3e175ed838d1c
DubnitskayaTatyana <Babashefa@yandex.ru>
2022-10-03 11:23:04 +0300

    editing game.ru, game.ipynb

--- Проект/game.py
+++ Проект/game.py
@@ -3,7 +3,7 @@ number = np.random.randint(1, 101) # загадываем число
 count = 0
 while True:
     count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
+    predict_number = int(input("Угадай число от 1 до 100:"))
 
     if predict_number > number:
         print("Число должно быть меньше!")
@@ -12,5 +12,5 @@ while True:
         print("Число должно быть больше!")
 
     else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        print(f"Вы угадали число! Это число: {number}, за {count} попыток")
         break # конец игры, выход из цикла
